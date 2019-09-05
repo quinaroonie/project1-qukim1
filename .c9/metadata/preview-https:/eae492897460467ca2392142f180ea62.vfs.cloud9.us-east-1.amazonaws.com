959b4df95c027704d8d5d25ae8955b4948b1d570
Ubuntu <ubuntu@ip-172-31-22-202.ec2.internal>
2019-09-05 03:51:00 +0000

    added artist

---
+++ .c9/metadata/preview-https:/eae492897460467ca2392142f180ea62.vfs.cloud9.us-east-1.amazonaws.com
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://eae492897460467ca2392142f180ea62.vfs.cloud9.us-east-1.amazonaws.com","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://eae492897460467ca2392142f180ea62.vfs.cloud9.us-east-1.amazonaws.com","previewer":"previewBrowser"},"timestamp":1567648087708,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- blue.py
+++ blue.py
@@ -8,9 +8,7 @@ app = flask.Flask(__name__)
 
 
 api_token = '3yEHJrYc03grMh7SUZsRlZKDlQv-3NK0EyJ3iZQquV69HlpaCU9p28OoUejFOLu5'
-
 url = "https://api.genius.com/search?q=Outkast"
-
 my_headers = {
     "Authorization": "Bearer qiICws1IclXFRZmTwrTJI7k4m8vWoOwy2smTaAmQ2RuyiWztfrPPijT9ea2i2nA-"
 }
@@ -19,31 +17,21 @@ response = requests.get(url, headers=my_headers)
 json_body = response.json()
 
 
-
 @app.route("/")  
 def index(): 
-    r = random.randint(1, 20)
+    r = random.randint(1, 10)
     photo = json_body["response"]["hits"][r]["result"]["song_art_image_url"]
     title = json_body["response"]["hits"][r]["result"]["full_title"]
-    artist = json_body["response"]["hits"][r]["result"]["image_url"]
+    artist = json_body["response"]["hits"][r]["result"]["primary_artist"]["image_url"]
+    
     return flask.render_template(
        "index.html",
         album_src=photo, title_src=title, artist_src=artist) 
-   
-        
-#    title = json_body["response"]["hits"][0]["result"]["full_title"]
-#    return flask.render_template(
-#        "index.html",
-#        title_src=title)
-    
-
-
+  
 app.run(
     port=int(os.getenv('PORT', 8080)),
     host=os.getenv('IP', '0.0.0.0')
 )
-
-
 
 
 """ 
